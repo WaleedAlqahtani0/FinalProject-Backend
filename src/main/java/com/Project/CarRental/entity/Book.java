8aca7445f1c0c0b8f03e9152202a1706a30bcf18
Waleed <waleed.alqahtani23@gmail.com>
2023-11-18 16:56:36 +0300

    Updete

--- src/main/java/com/Project/CarRental/entity/Book.java
+++ src/main/java/com/Project/CarRental/entity/Book.java
@@ -2,6 +2,7 @@ package com.Project.CarRental.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Objects;
 
@@ -12,6 +13,11 @@ public class Book {
     // --------- Primary key (courseId) generated automatically by the database
     @Id
     private String bookCode;
+
+    private int nationalID;
+    private String startDate;
+    private String endDate;
+    private String location;
 
     @ManyToOne
     @JoinColumn(name = "userId")
@@ -25,12 +31,53 @@ public class Book {
     private Payment payment;
 
 
+
     //---------- constructors, getters, setters, equals, hashCode and toString;
     public Book() {
     }
 
+    public Book(String bookCode, String startDate, String endDate, String location,int nationalID) {
+        this.bookCode = bookCode;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.nationalID=nationalID;
+    }
+
     public String getBookCode() {
         return bookCode;
+    }
+
+    public int getNationalID() {
+        return nationalID;
+    }
+
+    public void setNationalID(int nationalID) {
+        this.nationalID = nationalID;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public void setBookCode(String bookCode) {
